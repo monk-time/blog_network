daca2f0022c3342214a4458a0e6a2ba1eb63b4f8
Dmitry <monk-time@users.noreply.github.com>
2023-01-13 17:38:19 +0300

    Добавить urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    return HttpResponse('Главная страница социальной сети блогеров Yatube')
+
+
+def group_posts(request: HttpRequest, slug: str):
+    return HttpResponse(f'Посты группы "{slug}"')
