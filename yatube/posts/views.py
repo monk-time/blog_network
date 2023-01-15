9766370e623ef881bdfce5687b852a7f68842b3a
Dmitry <monk-time@users.noreply.github.com>
2023-01-15 17:18:26 +0300

    Выводить записи из БД на главную страницу

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from .models import Post
+
 
 def index(request: HttpRequest):
-    template = 'posts/index.html'
-    context = {'title': 'Это главная страница проекта Yatube'}
-    return render(request, template, context)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': 'Это главная страница проекта Yatube',
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request: HttpRequest, slug: str):
