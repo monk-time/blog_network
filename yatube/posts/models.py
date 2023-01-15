e845be696d96b4c5c671c38a3e03137b97b7ccd2
Dmitry <monk-time@users.noreply.github.com>
2023-01-15 22:29:42 +0300

    Поле slug модели Group должно быть уникальным

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
