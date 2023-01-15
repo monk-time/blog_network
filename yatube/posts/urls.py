f12d163144e620950b3f9379c42f810c24c4fc52
Dmitry <monk-time@users.noreply.github.com>
2023-01-15 22:07:46 +0300

    Переименовать псевдоним url главной страницы

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index, name='main_page'),
+    path('', views.index, name='index'),
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
 ]
