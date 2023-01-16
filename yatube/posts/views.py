e0f4f681ad74fb7d9ff062677f901ce5228b4f9e
Dmitry <monk-time@users.noreply.github.com>
2023-01-16 15:53:01 +0300

    Перенести заголовки внутрь шаблонов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,20 +6,15 @@ from .models import Group, Post
 
 def index(request: HttpRequest):
     posts = Post.objects.order_by('-pub_date')[:10]
-    context = {
-        'posts': posts,
-        'title': 'Последние обновления на сайте',
-    }
+    context = {'posts': posts}
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request: HttpRequest, slug: str):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    title = f'Записи сообщества "{group.title}"'
     context = {
         'group': group,
         'posts': posts,
-        'title': title,
     }
     return render(request, 'posts/group_list.html', context)
