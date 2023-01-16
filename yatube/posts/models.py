d45b1293db539f142f6eb6443b82b86df108228f
Dmitry <monk-time@users.noreply.github.com>
2023-01-16 15:48:43 +0300

    Не удалять посты при удалении группы

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name='posts',
         blank=True,
         null=True,
