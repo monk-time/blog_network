629779fba014ce3e588cd4fed3d5b0b29b62eb77
Dmitry <monk-time@users.noreply.github.com>
2023-01-17 17:59:41 +0300

    Добавить управляющую модель для `Group`

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -11,5 +11,11 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'description')
+    list_editable = ('slug',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
