8f41f7aa85303420b7f166a82bbf7cdafd188173
Dmitry <monk-time@users.noreply.github.com>
2023-01-19 14:37:06 +0300

    Регистрировать управляющие модели через декораторы

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Group, Post
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'text', 'pub_date', 'author', 'group')
     list_editable = ('group',)
@@ -11,11 +12,8 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+@admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'description')
     list_editable = ('slug',)
     empty_value_display = '-пусто-'
-
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Group, GroupAdmin)
