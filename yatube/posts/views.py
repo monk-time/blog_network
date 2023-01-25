5921e6f7e14f378769ac4d0bd46880dc9a25209b
Dmitry <monk-time@users.noreply.github.com>
2023-01-25 21:01:19 +0300

    Исправить исп. настроек из settings.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,21 @@
+from django.conf import settings
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
-
-from yatube.settings import POSTS_PER_PAGE
 
 from .models import Group, Post
 
 
 def index(request: HttpRequest):
-    posts = Post.objects.select_related('group').all()[:POSTS_PER_PAGE]
+    posts = Post.objects.select_related('group').all()[
+        : settings.POSTS_PER_PAGE
+    ]
     context = {'posts': posts}
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request: HttpRequest, slug: str):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POSTS_PER_PAGE]  # type: ignore
+    posts = group.posts.all()[: settings.POSTS_PER_PAGE]  # type: ignore
     context = {
         'group': group,
         'posts': posts,
