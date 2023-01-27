63728763fa333562f9b53ca3fbfd22e5bd60114f
Dmitry <monk-time@users.noreply.github.com>
2023-01-27 17:20:48 +0300

    Создание формы регистрации

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
+from . import views
+
 app_name = 'users'
 
 urlpatterns = [
@@ -9,4 +11,5 @@ urlpatterns = [
         LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout',
     ),
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
