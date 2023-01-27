e367cef5de1ac2fc73e88f804ab4e4fa9f5be5d5
Dmitry <monk-time@users.noreply.github.com>
2023-01-27 15:22:08 +0300

    Добавить новое приложение `users`

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
