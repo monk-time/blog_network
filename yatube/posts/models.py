795af5e097ae94c9a57b98b62848d52b2629a375
Dmitry <monk-time@users.noreply.github.com>
2023-01-27 14:31:16 +0300

    Изменить вывод постов

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -39,3 +39,6 @@ class Post(models.Model):
         ordering = ['-pub_date']
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
+
+    def __str__(self):
+        return self.text
