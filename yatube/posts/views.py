51cc505073c9ae777231092d3d31515caca84a2a
Dmitry <monk-time@users.noreply.github.com>
2023-01-27 11:02:37 +0300

    Джойнить таблицу с авторами

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,14 +6,18 @@ from .models import Group, Post
 
 
 def index(request: HttpRequest):
-    posts = Post.objects.select_related('group')[: settings.POSTS_PER_PAGE]
+    posts = Post.objects.select_related('group', 'author')[
+        : settings.POSTS_PER_PAGE
+    ]
     context = {'posts': posts}
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request: HttpRequest, slug: str):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[: settings.POSTS_PER_PAGE]  # type: ignore
+    posts = group.posts.select_related('author')[  # type: ignore
+        : settings.POSTS_PER_PAGE
+    ]
     context = {
         'group': group,
         'posts': posts,
