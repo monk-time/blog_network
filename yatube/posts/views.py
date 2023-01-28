d35495e2ae75751e66a6ba0f4df6e116c64ea676
Dmitry <monk-time@users.noreply.github.com>
2023-01-28 16:31:33 +0300

    Добавить паджинацию

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,25 +1,29 @@
 from django.conf import settings
+from django.core.paginator import Page, Paginator
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
 
+def paginate(request: HttpRequest, posts) -> Page:
+    """Разбить набор постов на страницы и вернуть запрашиваемую страницу."""
+    paginator = Paginator(posts, settings.POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
+
+
 def index(request: HttpRequest):
-    posts = Post.objects.select_related('group', 'author')[
-        : settings.POSTS_PER_PAGE
-    ]
-    context = {'posts': posts}
+    posts = Post.objects.select_related('group', 'author')
+    context = {'page': paginate(request, posts)}
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request: HttpRequest, slug: str):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')[  # type: ignore
-        : settings.POSTS_PER_PAGE
-    ]
+    posts = group.posts.select_related('author')  # type: ignore
     context = {
         'group': group,
-        'posts': posts,
+        'page': paginate(request, posts),
     }
     return render(request, 'posts/group_list.html', context)
