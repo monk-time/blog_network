d81f8fab5cf7b23d51c23eb31a1344cb8dd0dd60
Dmitry <monk-time@users.noreply.github.com>
2023-02-11 16:26:24 +0300

    Написать тесты для форм

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,1 +1,75 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from ..models import Group, Post
+
+User = get_user_model()
+
+
+class PostFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(  # type: ignore
+            username='test_user'
+        )
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='test-slug',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            text='Заранее созданный пост',
+            author=cls.user,
+            group=cls.group,
+        )
+
+    def setUp(self):
+        self.authorized_client = Client()
+        self.authorized_client.force_login(PostFormTests.user)
+
+    def test_post_form_creates_new_post(self):
+        """При отправке валидной формы создаётся новый пост."""
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Созданный через форму пост',
+            'group': PostFormTests.group.pk,
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_create'), data=form_data, follow=True
+        )
+        self.assertRedirects(
+            response,
+            reverse(
+                'posts:profile',
+                kwargs={'username': PostFormTests.user.username},
+            ),
+        )
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+        self.assertTrue(
+            Post.objects.filter(
+                text='Созданный через форму пост',
+                author=PostFormTests.user,
+                group=PostFormTests.group,
+            ).exists()
+        )
+
+    def test_post_form_edits_existing_post(self):
+        """При отправке валидной формы редактируется существующий пост."""
+        form_data = {
+            'text': 'Отредактированный пост',
+            'group': PostFormTests.group.pk,
+        }
+        response = self.authorized_client.post(
+            reverse('posts:post_edit', kwargs={'post_id': 1}),
+            data=form_data,
+            follow=True,
+        )
+        self.assertRedirects(
+            response,
+            reverse('posts:post_detail', kwargs={'post_id': 1}),
+        )
+        edited_post = Post.objects.get(id=1)
+        self.assertEquals(edited_post.text, 'Отредактированный пост')
+        self.assertEquals(edited_post.group, PostFormTests.group)
---
+++ yatube/users/tests/test_forms.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class UserFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.form_data = {
+            'first_name': 'Test',
+            'last_name': 'User',
+            'username': 'test_user',
+            'email': 'test@test.com',
+            'password1': 'testpassword',
+            'password2': 'testpassword',
+        }
+
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_user_signing_up_creates_new_user(self):
+        users_count = User.objects.count()
+        response = self.guest_client.post(
+            reverse('users:signup'), data=UserFormTests.form_data, follow=True
+        )
+        self.assertRedirects(response, reverse('posts:index'))
+        self.assertEqual(User.objects.count(), users_count + 1)
+        self.assertTrue(User.objects.filter(username='test_user').exists())
+
+    def test_user_cant_signup_with_registered_email(self):
+        self.guest_client.post(
+            reverse('users:signup'), data=UserFormTests.form_data
+        )
+        users_count = User.objects.count()
+        response = self.guest_client.post(
+            reverse('users:signup'), data=UserFormTests.form_data
+        )
+        self.assertEqual(User.objects.count(), users_count)
+        self.assertFormError(
+            response,
+            form='form',
+            field='email',
+            errors='Этот адрес уже зарегистрирован',
+        )
