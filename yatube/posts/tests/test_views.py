00dc6536d5611dda72afd7d9013decf9689f22f1
Dmitry <monk-time@users.noreply.github.com>
2023-02-13 14:21:08 +0300

    Убрать захардкоденные значения из test_views.py

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -31,9 +31,11 @@ class PostPagesTests(TestCase):
                 author=cls.user,
                 group=cls.group,
             )
-            for i in range(cls.POSTS_CREATED)
+            for _ in range(cls.POSTS_CREATED)
         ]
         Post.objects.bulk_create(posts)
+        # bulk_create doesn't call save(), so pk are not set yet
+        cls.posts = Post.objects.all()
 
         cls.paginated_urls = {
             reverse('posts:index'): 'posts/index.html',
@@ -46,11 +48,11 @@ class PostPagesTests(TestCase):
         }
         cls.other_urls = {
             reverse(
-                'posts:post_detail', kwargs={'post_id': 1}
+                'posts:post_detail', kwargs={'post_id': cls.posts[0].pk}
             ): 'posts/post_detail.html',
             reverse('posts:post_create'): 'posts/create_post.html',
             reverse(
-                'posts:post_edit', kwargs={'post_id': 1}
+                'posts:post_edit', kwargs={'post_id': cls.posts[0].pk}
             ): 'posts/create_post.html',
         }
 
@@ -91,19 +93,24 @@ class PostPagesTests(TestCase):
             with self.subTest(url=url):
                 response = self.guest_client.get(url)
                 post = response.context['page_obj'][0]
-                self.assertEqual(post.text, 'Тестовый пост')
-                self.assertEqual(post.author.username, 'test_user')
-                self.assertEqual(post.group.slug, 'test-slug')
+                self.assertEqual(post.text, PostPagesTests.posts[0].text)
+                self.assertEqual(
+                    post.author.username, PostPagesTests.user.username
+                )
+                self.assertEqual(post.group.slug, PostPagesTests.group.slug)
 
     def test_post_detail_has_correct_context(self):
         """Страница поста сформирована с правильным контекстом."""
         response = self.guest_client.get(
-            reverse('posts:post_detail', kwargs={'post_id': 1})
+            reverse(
+                'posts:post_detail',
+                kwargs={'post_id': PostPagesTests.posts[0].pk},
+            )
         )
         post = response.context['post']
-        self.assertEqual(post.text, 'Тестовый пост')
-        self.assertEqual(post.author.username, 'test_user')
-        self.assertEqual(post.group.slug, 'test-slug')
+        self.assertEqual(post.text, PostPagesTests.posts[0].text)
+        self.assertEqual(post.author.username, PostPagesTests.user.username)
+        self.assertEqual(post.group.slug, PostPagesTests.group.slug)
 
     def test_post_create_form_has_correct_context(self):
         """Страница создания поста сформирована с правильным контекстом."""
@@ -123,17 +130,23 @@ class PostPagesTests(TestCase):
         контекстом.
         """
         response = self.authorized_client.get(
-            reverse('posts:post_edit', kwargs={'post_id': 1})
+            reverse(
+                'posts:post_edit',
+                kwargs={'post_id': PostPagesTests.posts[0].pk},
+            )
         )
         post = response.context['form'].instance
-        self.assertEqual(post.text, 'Тестовый пост')
-        self.assertEqual(post.author.username, 'test_user')
-        self.assertEqual(post.group.slug, 'test-slug')
+        self.assertEqual(post.text, PostPagesTests.posts[0].text)
+        self.assertEqual(post.author.username, PostPagesTests.user.username)
+        self.assertEqual(post.group.slug, PostPagesTests.group.slug)
 
     def test_post_group_contains_only_correct_posts(self):
         """Посты не попают в группу, для которой они не предназначены."""
         response = self.guest_client.get(
-            reverse('posts:group_list', kwargs={'slug': 'empty-group'})
+            reverse(
+                'posts:group_list',
+                kwargs={'slug': PostPagesTests.group_without_posts.slug},
+            )
         )
         posts = response.context['page_obj']
         self.assertEqual(len(posts), 0)
