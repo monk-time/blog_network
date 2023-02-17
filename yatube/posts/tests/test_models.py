0830b203efb2deebe924e939103a02ea12d88560
Dmitry <monk-time@users.noreply.github.com>
2023-02-18 02:24:22 +0300

    Тестировать обрезание длинного поста

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -13,14 +13,20 @@ class PostModelTest(TestCase):
             slug='test-slug',
             description='Тестовое описание',
         )
-        cls.post = Post.objects.create(
+        cls.short_post = Post.objects.create(
             author=cls.user,
             text='Тестовый пост',
+        )
+        cls.long_post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост с очень длинным содержимым',
         )
 
     def test_models_have_correct_object_names(self):
         """Проверяем, что у моделей корректно работает __str__."""
-        post = PostModelTest.post
+        post = PostModelTest.short_post
         self.assertEqual(str(post), 'Тестовый пост')
+        post = PostModelTest.long_post
+        self.assertEqual(str(post), 'Тестовый пост с')
         group = PostModelTest.group
         self.assertEqual(str(group), 'Тестовая группа')
