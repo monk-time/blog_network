e296a49298db084c1288d498e6496dbb4ba84235
Dmitry <monk-time@users.noreply.github.com>
2023-02-19 19:41:42 +0300

    Использовать class-based views

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -5,15 +5,41 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list'),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('create/', views.post_create, name='post_create'),
-    path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
-    path('posts/<int:post_id>/delete/', views.post_delete, name='post_delete'),
+    path('', views.IndexView.as_view(), name='index'),
     path(
-        'posts/<int:post_id>/comment/', views.add_comment, name='add_comment'
+        'group/<slug:slug>/',
+        views.GroupView.as_view(),
+        name='group_list',
+    ),
+    path(
+        'profile/<str:username>/',
+        views.ProfileView.as_view(),
+        name='profile',
+    ),
+    path(
+        'posts/<int:post_id>/',
+        views.PostDetail.as_view(),
+        name='post_detail',
+    ),
+    path(
+        'create/',
+        views.PostCreate.as_view(),
+        name='post_create',
+    ),
+    path(
+        'posts/<int:post_id>/edit/',
+        views.PostEdit.as_view(),
+        name='post_edit',
+    ),
+    path(
+        'posts/<int:post_id>/delete/',
+        views.PostDelete.as_view(),
+        name='post_delete',
+    ),
+    path(
+        'posts/<int:post_id>/comment/',
+        views.AddComment.as_view(),
+        name='add_comment',
     ),
     path('follow/', views.follow_index, name='follow_index'),
     path(
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,115 +1,139 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.views import generic
 
 from .forms import CommentForm, PostForm
-from .models import Follow, Group, Post, User
+from .models import Comment, Follow, Group, Post, User
 from .utils import paginate
 
 
-def index(request: HttpRequest):
-    posts = Post.objects.select_related('group', 'author')
-    context = {'page_obj': paginate(request, posts)}
-    return render(request, 'posts/index.html', context)
+class IndexView(generic.ListView):
+    model = Post
+    template_name = 'posts/index.html'
+    queryset = Post.objects.select_related('group', 'author')
+    paginate_by = settings.POSTS_PER_PAGE
 
 
-def group_posts(request: HttpRequest, slug: str):
-    group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.select_related('author')  # type: ignore
-    context = {
-        'group': group,
-        'page_obj': paginate(request, posts),
-    }
-    return render(request, 'posts/group_list.html', context)
+class GroupView(generic.ListView):
+    model = Post
+    template_name = 'posts/group_list.html'
+    paginate_by = settings.POSTS_PER_PAGE
+
+    def setup(self, request, slug):
+        super().setup(request)
+        self.group = get_object_or_404(Group, slug=slug)
+
+    def get_queryset(self):
+        return self.group.posts.select_related('author')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['group'] = self.group
+        return context
 
 
-def profile(request: HttpRequest, username: str):
-    author = get_object_or_404(User, username=username)
-    posts = author.posts.select_related('group')  # type: ignore
-    following = (
-        request.user.is_authenticated
-        and author.following.filter(user=request.user).exists()  # type: ignore
-    )
-    context = {
-        'author': author,
-        'page_obj': paginate(request, posts),
-        'following': following,
-    }
-    return render(request, 'posts/profile.html', context)
+class ProfileView(generic.ListView):
+    model = Post
+    template_name = 'posts/profile.html'
+    paginate_by = settings.POSTS_PER_PAGE
 
-
-def post_detail(request: HttpRequest, post_id: int):
-    post = get_object_or_404(Post, pk=post_id)
-    context = {
-        'post': post,
-        'comments': post.comments.all(),  # type: ignore
-        'form': CommentForm(),
-    }
-    return render(request, 'posts/post_detail.html', context)
-
-
-@login_required
-def post_create(request: HttpRequest):
-    post = Post(author=request.user)
-    form = PostForm(
-        request.POST or None,
-        instance=post,
-        files=request.FILES or None,
-    )
-    if not form.is_valid():
-        return render(request, 'posts/create_post.html', {'form': form})
-
-    form.save()
-    return redirect(
-        'posts:profile',
-        username=request.user.username,  # type: ignore
-    )
-
-
-@login_required
-def post_edit(request: HttpRequest, post_id: int):
-    post = get_object_or_404(Post, pk=post_id)
-    if request.user != post.author:
-        return redirect(post)
-
-    form = PostForm(
-        request.POST or None,
-        instance=post,
-        files=request.FILES or None,
-    )
-    if not form.is_valid():
-        return render(
-            request, 'posts/create_post.html', {'form': form, 'is_edit': True}
+    def setup(self, request, username):
+        super().setup(request)
+        self.author = get_object_or_404(User, username=username)
+        self.following = (
+            self.request.user.is_authenticated
+            and self.author.following.filter(user=self.request.user).exists()
         )
 
-    form.save()
-    return redirect(post)
+    def get_queryset(self):
+        return self.author.posts.select_related('group')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author'] = self.author
+        context['following'] = self.following
+        return context
 
 
-@login_required
-def post_delete(request: HttpRequest, post_id: int):
-    post = get_object_or_404(Post, pk=post_id)
-    if request.user != post.author:
-        return redirect(post)
-    if request.method == 'POST':
-        post.delete()
-        return redirect(
-            'posts:profile',
-            username=request.user.username,  # type: ignore
-        )
-    return render(request, 'posts/post_delete_confirm.html')
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
+    pk_url_kwarg = 'post_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = self.object.comments.all()
+        context['form'] = CommentForm()
+        return context
 
 
-@login_required
-def add_comment(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    form = CommentForm(request.POST or None)
-    if form.is_valid():
-        comment = form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
-    return redirect(post)
+class PostCreate(LoginRequiredMixin, generic.CreateView):
+    form_class = PostForm
+    template_name = 'posts/create_post.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('posts:profile', args=[self.request.user.username])
+
+
+class PostEdit(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/create_post.html'
+    pk_url_kwarg = 'post_id'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            post = self.get_object()
+            return redirect(post)
+        return super().handle_no_permission()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_edit'] = True
+        return context
+
+
+class PostDelete(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Post
+    pk_url_kwarg = 'post_id'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            post = self.get_object()
+            return redirect(post)
+        return super().handle_no_permission()
+
+    def get_success_url(self):
+        return reverse('posts:profile', args=[self.request.user.username])
+
+
+class AddComment(LoginRequiredMixin, generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.author = self.request.user
+        form.instance.post = post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('posts:profile', args=[self.request.user.username])
 
 
 @login_required
