462116523e69f89d875c50afc0ef73f5749bdbdb
Dmitry <monk-time@users.noreply.github.com>
2023-02-28 17:54:42 +0300

    Мелкий рефактор вьюхи

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -124,7 +124,7 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if request.user.username != username:
+    if request.user != author:
         Follow.objects.get_or_create(user=request.user, author=author)
     return redirect('posts:profile', username=username)
 
